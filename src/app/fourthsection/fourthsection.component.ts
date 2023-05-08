cf1e5ff509a38a2840060298aeea71bc0d8fab9e
batuhansicakyuz <batuhansicakyuz@gmail.com>
2023-05-08 22:47:15 +0300

    fourth

---
+++ src/app/fourthsection/fourthsection.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fourth-section',
+  templateUrl: './fourthsection.component.html',
+  styleUrls: ['./fourthsection.component.css']
+})
+export class FourthSection {
+
+}
