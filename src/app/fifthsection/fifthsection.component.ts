b7e7d30c4f5c548776012abd48f53ba03abd6b1b
batuhansicakyuz <batuhansicakyuz@gmail.com>
2023-05-31 14:57:54 +0300

    google maps added

--- src/app/fifthsection/fifthsection.component.ts
+++ src/app/fifthsection/fifthsection.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./fifthsection.component.css']
 })
 export class FifthSection {
-
+mapOptions: google.maps.MapOptions = {
+    center: { lat: 41.02501915316403, lng: 28.981049711793283 },
+    zoom: 14,
+    zoomControl: false,
+       mapTypeControl: false,
+       streetViewControl: false,
+       fullscreenControl: false
+  };
 }
